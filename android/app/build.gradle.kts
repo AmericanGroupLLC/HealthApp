6490c4da96881cc84b231771675a951b458adbe7
Srikanth Patchava <spatchava@meta.com>
2026-05-13 19:07:45 -0700

    Disable R8 minification for initial release (debug with CI logs later)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -76,8 +76,8 @@ android {
             enableAndroidTestCoverage = true
         }
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             signingConfig = if (hasReleaseKeystore) {
                 signingConfigs.getByName("release")
